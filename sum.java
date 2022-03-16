eb8ed63ecacc1331e8c59c9705037cf72617433c
Ashvani Uttam <techstudy204@gmail.com>
2022-03-16 14:59:57 +0530

    java programming

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String[] args) {
+        int n1 = 45;
+        int n2 = 56;
+        int sum;
+        sum = n1 + n2;
+        System.out.println("sum of two numbers = " + sum);
+    }
+
+}
