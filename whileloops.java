63bd1601213fcf89edaa39e1b94b45a0a0c20d42
Ashvani Uttam <techstudy204@gmail.com>
2022-03-19 15:37:57 +0530

    while loop

---
+++ whileloops.java
@@ -0,0 +1,15 @@
+public class whileloops {
+
+    public static void main(String[] args) {
+        /*
+         * boolean myLoop = 25 > 35;
+         * System.out.println(myLoop);
+         */
+        int num = 23;
+        while (num <= 34) {
+            System.out.println("okay" + " " + num);
+            num++;
+        }
+    }
+
+}
