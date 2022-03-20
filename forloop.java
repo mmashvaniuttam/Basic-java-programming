793ce5ae551034799cde809a72e86ba9d5c39cd2
Ashvani Uttam <techstudy204@gmail.com>
2022-03-20 09:42:38 +0530

    for loop1

--- forloop.java
+++ forloop.java
@@ -4,9 +4,9 @@
 public class forloop {
 
     public static void main(String[] args) {
-        int i = 0;
-        for (i = 0; i < 10; i++) {
-            System.out.println(i);
+        // int i = 0;
+        for (int i = 0; i < 10; i++) {
+            System.out.printf(" My variable value is: %d\n", i);
         }
     }
 }
