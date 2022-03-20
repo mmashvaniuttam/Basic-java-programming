ddb81d21470f455cd658b6295da79403c475e346
Ashvani Uttam <techstudy204@gmail.com>
2022-03-20 09:59:14 +0530

    if statement

---
+++ if_statement.java
@@ -0,0 +1,12 @@
+/**
+ * if_statement
+ */
+public class if_statement {
+
+    public static void main(String[] args) {
+        if (3 != 10) {
+            System.out.println("it's true");
+        }
+
+    }
+}
--- string.java
+++ string.java
@@ -1,10 +1,8 @@
-import java.time.format.TextStyle;
-
 public class string {
     public static void main(String[] args) {
         String text = "Ashvani";
         String blank = " ";
-        String moreText = "Uttam";    /*if we want more string then we can moreText */
+        String moreText = "Uttam"; /* if we want more string then we can moreText */
         System.out.println(text + blank + moreText);
     }
 
