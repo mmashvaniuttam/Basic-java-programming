a9affa9ed7191213f8d13e1d5364367c3704361e
Ashvani Uttam <techstudy204@gmail.com>
2022-03-22 20:48:34 +0530

    Array java

---
+++ array_2.java
@@ -0,0 +1,14 @@
+public class array_2 {
+    public static void main(String[] args) {
+        int a[] = new int[5]; // declaration and instantiation
+        a[0] = 34;
+        a[1] = 45;
+        a[2] = 56;
+        a[3] = 76;
+        a[4] = 23;
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
