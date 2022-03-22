84e45855a65ce0a342150c7f0688a650c655bc5f
Ashvani Uttam <93753746+mmashvaniuttam@users.noreply.github.com>
2022-03-22 20:11:18 +0530

    Update switch_statement.java

--- switch_statement.java
+++ switch_statement.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 /**
  * switch_statement
+ variable tpye supported by switch statement:-char, short, byte, int, string.
  */
 public class switch_statement {
     public static void main(String[] args) {
